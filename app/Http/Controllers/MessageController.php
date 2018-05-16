8c48e7f4dc50ee2e02a18683c5b989d61575a75e
Your Name <you@example.com>
2018-05-16 21:48:27 +0300

    Initial commit

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Events\MessageSent;
+use App\Message;
+use Illuminate\Support\Facades\Auth;
+
+class MessageController extends Controller
+{
+
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show chats
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('chat');
+    }
+
+    /**
+     * Fetch all messages
+     *
+     * @return Message
+     */
+    public function fetchMessages()
+    {
+        return Message::with('user')->get();
+    }
+
+    /**
+     * Persist message to database
+     *
+     * @param  Request $request
+     * @return Response
+     */
+    public function sendMessage(Request $request)
+    {
+
+
+        $user = Auth::user();
+
+        $message = $user->messages()->create([
+            'message' => $request->input('message')
+        ]);
+
+        broadcast(new MessageSent($user, $message))->toOthers();
+
+        response()->json([
+            'status' => 'Message Sent!'
+        ]);
+    }
+}
